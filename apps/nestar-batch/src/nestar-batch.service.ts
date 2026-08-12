2ec011123028e582e46e02c9983277f2c6e9bebe
waheddevs <abduvohid_gulomov@mail.ru>
2026-08-12 18:19:55 +0900

    feat: integration GraphQL Module & build first GraphQL api

--- apps/nestar-batch/src/nestar-batch.service.ts
+++ apps/nestar-batch/src/nestar-batch.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class NestarBatchService {
-  getHello(): string {
-    return 'Welcome to Nestar Batch Server!';
-  }
+	getHello(): string {
+		return 'Welcome to Nestar Batch Server!';
+	}
 }
