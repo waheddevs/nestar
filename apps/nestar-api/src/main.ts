9b340f5a142aff77a7e5e6cb34d3113d928899fb
waheddevs <abduvohid_gulomov@mail.ru>
2026-08-19 17:12:26 +0900

    feat: standard integration of logging interceptor

--- apps/nestar-api/src/main.ts
+++ apps/nestar-api/src/main.ts
@@ -2,6 +2,7 @@ import * as dns from 'dns';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { LoggingInterceptor } from './libs/interceptor/Logging.interceptor';
 
 // Ba'zi wi-fi router DNS'lari MongoDB Atlas'ning SRV so'rovini bloklaydi
 // (querySrv ECONNREFUSED). Node resolverini barqaror DNS'ga qaratamiz.
@@ -10,6 +11,7 @@ dns.setServers(['8.8.8.8', '1.1.1.1']);
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 	app.useGlobalPipes(new ValidationPipe());
+	app.useGlobalInterceptors(new LoggingInterceptor());
 	await app.listen(process.env.PORT_API ?? 3000);
 }
 bootstrap();
