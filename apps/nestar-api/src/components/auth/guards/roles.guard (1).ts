c619a3f39748ded29b76a4c1110e66a0f43ef19f
waheddevs <abduvohid_gulomov@mail.ru>
2026-08-21 16:28:28 +0900

    feat: create checkAuthRoles for testing

--- apps/nestar-api/src/components/auth/guards/roles.guard (1).ts
+++ apps/nestar-api/src/components/auth/guards/roles.guard (1).ts
@@ -1,7 +1,7 @@
 import { BadRequestException, CanActivate, ExecutionContext, Injectable, ForbiddenException } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { AuthService } from '../auth.service';
-import { Message } from 'apps/nestar-api/src/libs/enums/common.enum';
+import { Message } from '../../../libs/enums/common.enum';
 
 @Injectable()
 export class RolesGuard implements CanActivate {
--- apps/nestar-api/src/components/member/member.resolver.ts
+++ apps/nestar-api/src/components/member/member.resolver.ts
@@ -6,6 +6,9 @@ import { UseGuards } from '@nestjs/common';
 import { AuthGuard } from '../auth/guards/auth.guard (1)';
 import { AuthMember } from '../auth/decorators/authMember.decorator';
 import { ObjectId } from 'mongoose';
+import { Roles } from '../auth/decorators/roles.decorator (1)';
+import { MemberType } from '../../libs/enums/member.enum';
+import { RolesGuard } from '../auth/guards/roles.guard (1)';
 
 @Resolver()
 export class MemberResolver {
@@ -38,6 +41,14 @@ export class MemberResolver {
 		return `Hi ${memberNick}`;
 	}
 
+	@Roles(MemberType.USER, MemberType.AGENT)
+	@UseGuards(RolesGuard)
+	@Query(() => String)
+	public async checkAuthRoles(@AuthMember() authMember: Member): Promise<String> {
+		console.log('Mutation, checkAuthRoles');
+		return `Hi ${authMember.memberNick} , you are ${authMember.memberType}`;
+	}
+
 	@Query(() => String)
 	public async getMember(): Promise<String> {
 		console.log('Mutation, getMember');
@@ -46,7 +57,8 @@ export class MemberResolver {
 
 	/** ADMIN **/
 
-	// Authorization: ADMIN
+	@Roles(MemberType.ADMIN)
+	@UseGuards(RolesGuard)
 	@Mutation(() => String)
 	public async getAllMembersByAdmin(): Promise<string> {
 		return await this.memberService.getAllMembersByAdmin();
