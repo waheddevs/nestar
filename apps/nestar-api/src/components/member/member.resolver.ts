7814fef856443a47dd91d05449dd015a3b48dfa6
waheddevs <abduvohid_gulomov@mail.ru>
2026-08-28 11:00:27 +0900

    feat: develop getAgents graphQl api

--- apps/nestar-api/src/components/member/member.resolver.ts
+++ apps/nestar-api/src/components/member/member.resolver.ts
@@ -1,7 +1,7 @@
 import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { MemberService } from './member.service';
-import { LoginInput, MemberInput } from '../../libs/dto/member/member.input';
-import { Member } from '../../libs/dto/member/member';
+import { AgentsInquiry, LoginInput, MemberInput } from '../../libs/dto/member/member.input';
+import { Member, Members } from '../../libs/dto/member/member';
 import { UseGuards } from '@nestjs/common';
 import { AuthGuard } from '../auth/guards/auth.guard (1)';
 import { AuthMember } from '../auth/decorators/authMember.decorator';
@@ -62,6 +62,12 @@ export class MemberResolver {
 		console.log('memberId', memberId);
 		const targetId = shapeIntoMongoObjectId(input);
 		return await this.memberService.getMember(memberId, targetId);
+	}
+
+	@UseGuards(WithoutGuard)
+	@Query(() => Members)
+	public async getAgents(@Args('input') input: AgentsInquiry, @AuthMember('_id') memberId: ObjectId): Promise<Members> {
+		return this.memberService.getAgents(memberId, input);
 	}
 
 	/** ADMIN **/
