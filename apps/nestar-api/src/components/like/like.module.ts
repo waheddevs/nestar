124e1ad693a3760bc5a9997b353f9a1bd7527916
waheddevs <abduvohid_gulomov@mail.ru>
2026-09-04 09:27:22 +0900

    feat: integrate like module related to DTO

--- apps/nestar-api/src/components/like/like.module.ts
+++ apps/nestar-api/src/components/like/like.module.ts
@@ -1,4 +1,11 @@
 import { Module } from '@nestjs/common';
+import { LikeResolver } from './like.resolver';
+import { LikeService } from './like.service';
+import { MongooseModule } from 'node_modules/@nestjs/mongoose/dist/mongoose.module';
+import LikeSchema from '../../schemas/Like.model';
 
-@Module({})
+@Module({
+	imports: [MongooseModule.forFeature([{ name: 'Like', schema: LikeSchema }])],
+	providers: [LikeResolver, LikeService],
+})
 export class LikeModule {}
